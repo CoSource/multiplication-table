28bee073095a78393dc616d55a072d1ec121e41c
cosource <richard@cosource.com.au>
2015-02-23 10:51:52 +0100

    Initial Release

---
+++ myApp.js
@@ -0,0 +1,22 @@
+angular.module('myApp', [])
+    .controller('MultiplicationCtrl', function($scope, $attrs) {
+        function populateNumbers(x) {
+            var numbers = [];
+            for(var i=0; i<x; i++) {
+                numbers[i] = i + 1; 
+            };
+            return numbers;
+        }
+        $scope.numberLimit = $attrs.initialNumberLimit || 10;
+		
+		$scope.$watch('numberLimit', function(limit) {
+			$scope.numbers = populateNumbers(limit);
+		});
+		
+		$scope.compute = function(a,b) {
+			return a * b;
+		};
+		
+		
+		
+    });
